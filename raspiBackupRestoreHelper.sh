943e5a4b14a52b3fc2e650dd3cf2060675debe19
franjo_G <pi@fgreufe.de>
2022-03-18 13:24:31 +0100

    test

--- raspiBackupRestoreHelper.sh
+++ raspiBackupRestoreHelper.sh
@@ -31,7 +31,6 @@ normal='\033[0;39m'                #Font colors for output
 red='\033[1;31m'
 yellow='\033[1;33m'
 green='\033[1;32m'
-green='\033[1;32m'
 
 hostname="$(hostname)"           #Determine hostname
 
